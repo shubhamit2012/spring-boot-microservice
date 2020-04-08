609d4c167edc7faddc5faa627f3bce0ba0d7d830
Shubham Srivastava <shubhamsrivastava@Shubhams-MacBook-Pro.local>
2020-04-09 01:19:25 +0530

    #1 Implemented EurekaServiceDiscovery server

---
+++ photoapp-discovery-service/src/test/java/com/draco/photoapp/discovery/PhotoappDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draco.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
